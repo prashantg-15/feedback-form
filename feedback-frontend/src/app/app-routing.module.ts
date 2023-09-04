9b9309c021d1610170de5acf7b42c3b2fd7cf904
Prashant Gowda <prashant.gowda@spit.ac.in>
2023-09-04 20:20:38 +0530

    Feedback form added

--- feedback-frontend/src/app/app-routing.module.ts
+++ feedback-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TeacherFeedbackComponent } from './teacher-feedback/teacher-feedback.component';
+import { CurriculumFeedbackComponent } from './curriculum-feedback/curriculum-feedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: HomePageComponent
+  },
+  { 
+    path: 'teacher-feedback', 
+    component: TeacherFeedbackComponent 
+  },
+  { 
+    path: 'curriculum-feedback', 
+    component: CurriculumFeedbackComponent 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ feedback-frontend/src/app/teacher-feedback/teacher-feedback.component.ts
@@ -0,0 +1,171 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-teacher-feedback',
+  templateUrl: './teacher-feedback.component.html',
+  styleUrls: ['./teacher-feedback.component.scss']
+})
+export class TeacherFeedbackComponent {
+
+  PR!: FormGroup;
+  HK!: FormGroup;
+  NM!: FormGroup;
+  AK!: FormGroup;
+  PT!: FormGroup;
+
+  PR_step = false;
+  HK_step = false;
+  NM_step = false;
+  AK_step = false;
+  PT_step = false;
+
+  step = 1;
+  feedback: any = [];
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.PR = this.formBuilder.group({
+      ucid: [''],
+      Q1: [''],
+      Q2: [''],
+      Q3: [''],
+      Q4: [''],
+      Q5: [''],
+      Q6: [''],
+      Q7: [''],
+      Q8: [''],
+      Q9: [''],
+      Q10: [''],
+      Q11: [''],
+      faculty: ['Prof. Dr. Pooja Raundale']
+    });
+
+    this.HK = this.formBuilder.group({
+      ucid: [''],
+      Q1: [''],
+      Q2: [''],
+      Q3: [''],
+      Q4: [''],
+      Q5: [''],
+      Q6: [''],
+      Q7: [''],
+      Q8: [''],
+      Q9: [''],
+      Q10: [''],
+      Q11: [''],
+      faculty: ['Prof. Harshil Kanakia']
+    });
+
+    this.NM = this.formBuilder.group({
+      ucid: [''],
+      Q1: [''],
+      Q2: [''],
+      Q3: [''],
+      Q4: [''],
+      Q5: [''],
+      Q6: [''],
+      Q7: [''],
+      Q8: [''],
+      Q9: [''],
+      Q10: [''],
+      Q11: [''],
+      faculty: ['Prof. Nikhita Mangaonkar']
+    });
+
+    this.AK = this.formBuilder.group({
+      ucid: [''],
+      Q1: [''],
+      Q2: [''],
+      Q3: [''],
+      Q4: [''],
+      Q5: [''],
+      Q6: [''],
+      Q7: [''],
+      Q8: [''],
+      Q9: [''],
+      Q10: [''],
+      Q11: [''],
+      faculty: ['Prof. Aarti Karande']
+    });
+
+    this.PT = this.formBuilder.group({
+      ucid: [''],
+      Q1: [''],
+      Q2: [''],
+      Q3: [''],
+      Q4: [''],
+      Q5: [''],
+      Q6: [''],
+      Q7: [''],
+      Q8: [''],
+      Q9: [''],
+      Q10: [''],
+      Q11: [''],
+      faculty: ['Prof. Pallavi Thakur']
+    });
+
+    this.PR.get('ucid')?.valueChanges.subscribe((newValue) => {
+      this.HK.get('ucid')?.setValue(newValue);
+      this.NM.get('ucid')?.setValue(newValue);
+      this.AK.get('ucid')?.setValue(newValue);
+      this.PT.get('ucid')?.setValue(newValue);
+    });
+  }
+
+  get pooja() { return this.PR.controls; }
+  get harshil() { return this.HK.controls; }
+  get nikhita() { return this.NM.controls; }
+  get aarti() { return this.AK.controls; }
+  get pallavi() { return this.PT.controls; }
+
+  next() {
+    if (this.step == 1) {
+      this.PR_step = true;
+      // if (this.HK.invalid) { return }
+    }
+    else if (this.step == 2) {
+      this.HK_step = true;
+    }
+    else if (this.step == 3) {
+      this.NM_step = true;
+    }
+    else if (this.step == 4) {
+      this.AK_step = true;
+    }
+    this.step++;
+  }
+
+  previous() {
+    this.step--;
+    if (this.step == 1) {
+      this.HK_step = false;
+    }
+    else if (this.step == 2) {
+      this.NM_step = false;
+    }
+    else if (this.step == 3) {
+      this.AK_step = false;
+    }
+    else if (this.step == 4) {
+      this.PT_step = false;
+    }
+  }
+
+  submit() {
+    if (this.step == 5) {
+      this.PT_step = true;
+      // if (this.educationalDetails.invalid) { return }
+      this.feedback.push(this.PR.value);
+      this.feedback.push(this.HK.value);
+      this.feedback.push(this.NM.value);
+      this.feedback.push(this.AK.value);
+      this.feedback.push(this.PT.value);
+      console.log(JSON.stringify(this.feedback));
+    }
+  }
+
+  
+
+}
